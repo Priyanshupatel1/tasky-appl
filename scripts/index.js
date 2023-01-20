33a20525e46a9421077f72988529653a9d0020a7
Priyanshupatel1 <Patelpriyanshu321@gmail.com>
2023-01-20 23:54:15 +0530

    added container for our new app

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const state={
+     taskList: [],
+};
+//dom manipulation
+const taskmodel=document.querySelector(".task__modal__body");
+
+//to create a cardr in homepage
+const htmlTaskContent=({id,title,description,type,url})=>`
+<div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
+     <div class='card shadow-sm task__card'>
+          <div class='card-header d-flex justify-content-end task__card__header'>
+               <button type='button' class='btn btn-outline-info mr-2' name=${id}>
+               <i class='fa fa-pencil-alt' name='${id}'></i>
+               </button>
+               <div class='card-body'>
+                    ${
+                         url &&
+                         `<img width=100%' src=${url} alt='card image here' class='card-image-top md-3 rounded-lg'/>`
+                    }
+                    <h4 class ='task__card__title'>${title}</h4>
+                    <p class='description trim-3-lines text-muted'data-gram-editor='false'>${description}</p>
+                    <div class='tags text-white d-flex flex-wrap'>
+                         <span class='badge bg-primary m-1'>${type}</span>
+                    </div>
+               </div>
+               <div class='card-footer'>
+               <button type='button' class='btn btn-outline-primary float-right data-bs-toggle="modal" data-bs-target="showTask">open task</button>
+          </div>
+     </div>
+</div>
+`;
